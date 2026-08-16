ca815c452cd17e3efd61e2c10fff784748a87c9a
Athanatos123 <alain.delree@gmail.com>
2026-08-16 10:38:35 +0200

    fix: supprime les numeros de coups avant lecture TTS (issue #169, suite #168)

--- nicsoft/modes/opening_explorer/tts_engine.py
+++ nicsoft/modes/opening_explorer/tts_engine.py
@@ -80,6 +80,7 @@ def strip_markdown(text: str) -> str:
     text = re.sub(r'__(.+?)__', r'\1', text)
     text = re.sub(r'\*(.+?)\*', r'\1', text)
     text = re.sub(r'_(.+?)_', r'\1', text)
+    text = re.sub(r'\b\d+\.{1,3}\s*', '', text)
     text = re.sub(r'\n+', ' ', text)
     return text.strip()
 
